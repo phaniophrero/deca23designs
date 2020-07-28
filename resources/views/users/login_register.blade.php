a06433b7422545330c4869657a8e0192e0bf13ef
phaniophrero <phaniophrero@yahoo.com>
2020-07-28 03:54:23 +0200

    Updated Login form

--- resources/views/users/login_register.blade.php
+++ resources/views/users/login_register.blade.php
@@ -18,29 +18,31 @@
             </div>
             @endif
             <div class="auth-pages">
-                <div class="login-form auth-left">
-                    <!--login form-->
-                    <h2 class="register-heading">Login to your account</h2>
-                    <form id="loginForm" name="loginForm" action="{{ url('/user-login') }}" method="post">
-                        {{ csrf_field() }}
-                        <input id="email" name="email" type="email" placeholder="Email Address" required />
-                        <input id="password" name="password" type="password" placeholder="Password" required />
-                        <div class="login-container">
-                            <span>
-                                <input type="checkbox" class="checkbox">
-                                Keep me signed in
-                            </span>
-                            <button type="submit" class="auth-button">Login</button><br>
-                        </div>
-                        <a href="{{ url('forgot-password') }}" class="forgot-password-link">Forgot Password?</a>
-                    </form>
+                <div class="auth-left-bg">
+                    <div class="login-form auth-left">
+                        <!--login form-->
+                        <h2 class="register-heading">Login to your account</h2>
+                        <form id="loginForm" name="loginForm" action="{{ url('/user-login') }}" method="post">
+                            {{ csrf_field() }}
+                            <input id="email" name="email" type="email" placeholder="Email Address" required />
+                            <input id="password" name="password" type="password" placeholder="Password" required />
+                            <div class="login-container">
+                                <span class="round">
+                                    <input type="checkbox" class="checkbox" id="check">
+                                    <label for="check">Remember me</label>
+                                </span>
+                                <button type="submit" class="auth-button auth-btn-login">Login</button><br>
+                            </div>
+                            <a href="{{ url('forgot-password') }}" class="forgot-password-link">Forgot Password?</a>
+                        </form>
+                    </div>
                 </div>
                 <!--/login form-->
 
-                <div class="">
+                <div class="auth-right-bg">
                     <div class="signup-form auth-right">
                         <!--sign up form-->
-                        <h2 class="register-heading">Create an account</h2>
+                        <h2 class="register-heading2">Create an account</h2>
                         <form id="registerForm" name="registerForm" action="{{ url('/user-register') }}" method="post">
                             {{ csrf_field() }}
                             <input id="name" name="name" type="text" placeholder="Name" required />
